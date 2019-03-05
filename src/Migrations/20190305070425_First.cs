5cf7e32c1e7b15277569875d1cdac084832332b5
Fredrik Eilertsen <fredrik.eilertsen@gmail.com>
2019-03-05 08:05:13 +0100

    Add migrations

---
+++ src/Migrations/20190305070425_First.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DuaBot.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SlackUpdateTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Subject = table.Column<string>(nullable: true),
+                    TimeZone = table.Column<string>(nullable: true),
+                    SlackUserId = table.Column<string>(nullable: true),
+                    End = table.Column<DateTimeOffset>(nullable: false),
+                    Start = table.Column<DateTimeOffset>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SlackUpdateTasks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserTokens",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    SlackId = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    AccessToken = table.Column<string>(nullable: true),
+                    RefreshToken = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserTokens", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SlackUpdateTasks");
+
+            migrationBuilder.DropTable(
+                name: "UserTokens");
+        }
+    }
+}
